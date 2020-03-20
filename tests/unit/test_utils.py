c1fca9f520d2b18ddf98a20c410d746d16af92cf
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-03-20 17:07:07 +1100

    Add get_env utils

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+from openscm_runner.utils import get_env
+
+
+def test_get_env(monkeypatch):
+    expected_output = "hello"
+    monkeypatch.setenv("TESTVAR", expected_output)
+    assert get_env("TESTVAR") == expected_output
+
+def test_get_env_not_defined(monkeypatch):
+    monkeypatch.delenv("TESTVAR", raising=False)
+    with pytest.raises(ValueError, match="TESTVAR is not set"):
+        get_env("TESTVAR")
