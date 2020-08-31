f4512f8500c0ec74daaf10f0af84149b6864137e
Chris Smith <chrisroadmap@gmail.com>
2020-08-31 16:15:11 +0100

    increment minimum FaIR version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ SOURCE_DIR = "src"
 
 REQUIREMENTS = [
     "click",
-    "fair>=1.6.0c0,<2.0.0",
+    "fair>=1.6.0c1,<2.0.0",
     "matplotlib==3.2.2",
     "pyam-iamc",
     "pymagicc>=2.0.0rc4",
