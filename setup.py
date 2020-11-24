bdfda77fde8e2effb167b207bb03680748edc9fc
maritsandstad <marit.sandstad@astro.uio.no>
2020-11-24 15:34:40 +0100

    Playing around with setup file to include input data

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ REQUIREMENTS_EXTRAS = {
 # no tests/docs in `src` so don't need exclude
 PACKAGES = find_packages(SOURCE_DIR)
 PACKAGE_DIR = {"": SOURCE_DIR}
-PACKAGE_DATA = {"openscm_runner": [os.path.join("adapters", "fair_adapter", "*.csv")]}
+PACKAGE_DATA = {"openscm_runner": [os.path.join("adapters", "fair_adapter", "*.csv"), os.path.join("adapters", "ciceroscm_adapter", "utils_templates", "*.txt")]}
 
 # Get the long description from the README file
 with open(README, "r") as f:
