abcfb101fd5ac1036980fba40ddf116c56a252e4
Robert Gieseke <rob.g@web.de>
2020-12-08 11:03:16 +0100

    Add check of actual value

--- tests/unit/test_fair1x_utils.py
+++ tests/unit/test_fair1x_utils.py
@@ -1,3 +1,5 @@
+import numpy.testing as npt
+
 from openscm_runner.adapters.fair_adapter._scmdf_to_emissions import scmdf_to_emissions
 
 
@@ -8,4 +10,7 @@ from openscm_runner.adapters.fair_adapter._scmdf_to_emissions import scmdf_to_em
 def test_emissions_to_ignore(test_scenario_ssp370_world):
     emissions = scmdf_to_emissions(test_scenario_ssp370_world)
 
+    npt.assert_allclose(
+        emissions[0, 1], 2.59244179e-03, rtol=1e-5,
+    )
     assert emissions.shape[1] == 40
